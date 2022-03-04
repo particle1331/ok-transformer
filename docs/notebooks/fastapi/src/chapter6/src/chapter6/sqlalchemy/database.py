956547f694f5a46ed66777664fe7293ca9e7908d
particle <54845098+particle1331@users.noreply.github.com>
2022-03-04 21:10:15 +0800

    Done with basic SQL commands

--- docs/notebooks/fastapi/src/chapter6/src/chapter6/sqlalchemy/database.py
+++ docs/notebooks/fastapi/src/chapter6/src/chapter6/sqlalchemy/database.py
@@ -1,10 +1,11 @@
-import sqlalchemy
 from databases import Database
+from sqlalchemy import create_engine
+import pandas as pd
 
 
 DATABASE_URL = "sqlite:///chapter6_sqlalchemy.db"
 database = Database(DATABASE_URL)
-sqlalchemy_engine = sqlalchemy.create_engine(DATABASE_URL)
+sqlalchemy_engine = create_engine(DATABASE_URL)
 
 
 def get_database() -> Database:
