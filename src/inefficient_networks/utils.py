2f7056b14c440e20ed191aa67277432b38a078eb
particle <54845098+particle1331@users.noreply.github.com>
2022-05-01 01:13:48 +0800

    CNN + transfer learning

--- src/inefficient_networks/utils.py
+++ src/inefficient_networks/utils.py
@@ -27,3 +27,24 @@ def download_kaggle_competition(competition: str):
         os.system(f"kaggle competitions download -c {competition} -p {config.DATASET_DIR}")
         os.system(f"unzip {data_path}.zip -d {data_path} > /dev/null")
         os.system(f"rm {data_path}.zip")
+
+
+def plot_model_history(history, ax, metric='accuracy', label='', linestyle='solid'):
+    """Plotting result of Keras model training.
+    Example:
+    >>> fig, ax = plt.subplots(1, 2, figsize=(10, 4))
+    >>> plot_model_history(history=hist, ax=ax)
+    """
+
+    train_label = f'Train ({label})' if len(label) > 0 else 'Train'
+    valid_label = f'Valid ({label})' if len(label) > 0 else 'Valid'
+    ax[0].plot(history.history['loss'], label=train_label, color="C0", linestyle=linestyle)
+    ax[0].plot(history.history['val_loss'], label=valid_label, color="C1", linestyle=linestyle)
+    ax[0].set_ylabel('loss')
+    ax[0].legend()
+
+    ax[1].plot(history.history[metric], label=train_label, color="C0", linestyle=linestyle)
+    ax[1].plot(history.history[f'val_{metric}'], label=valid_label, color="C1", linestyle=linestyle)
+    ax[1].set_ylabel(metric)
+    ax[1].legend()
+    
