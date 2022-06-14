63c83cc3533d631a9171429f12d8164e484893bc
particle <54845098+particle1331@users.noreply.github.com>
2022-06-14 23:25:33 +0800

    incorporate bugfix to notebook.

--- docs/notebooks/mlops/3-prefect/deployments.py
+++ docs/notebooks/mlops/3-prefect/deployments.py
@@ -1,7 +1,7 @@
 from prefect.deployments import DeploymentSpec
 from prefect.orion.schemas.schedules import IntervalSchedule
 from prefect.flow_runners import SubprocessFlowRunner
-from datetime import timedelta, datetime
+from datetime import timedelta
 
 
 DeploymentSpec(
