7966aed26ff19ea50970b281b30f25d4093fdb2c
particle <54845098+particle1331@users.noreply.github.com>
2022-06-16 04:49:46 +0800

    Fix paths.

--- docs/notebooks/mlops/4-deployment/ride_model/train.py
+++ docs/notebooks/mlops/4-deployment/ride_model/train.py
@@ -31,7 +31,7 @@ def run_training(train_path, valid_path):
 
     # Persist trained pipeline
     pipeline = train_lr_pipeline(prepare_features(X_train), y_train)
-    joblib.dump(pipeline, 'pipeline.pkl')
+    joblib.dump(pipeline, package_dir / 'pipeline.pkl')
     
     # Evaluation
     X_valid = valid_data.drop(['duration'], axis=1)
@@ -39,3 +39,24 @@ def run_training(train_path, valid_path):
 
     print("RMSE (train):", mean_squared_error(y_train, pipeline.predict(prepare_features(X_train)), squared=False))
     print("RMSE (valid):", mean_squared_error(y_valid, pipeline.predict(prepare_features(X_valid)), squared=False))
+
+
+if __name__ == "__main__":
+    
+    from utils import package_dir
+    import argparse
+    import ssl
+    ssl._create_default_https_context = ssl._create_unverified_context
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--taxi_type", default='green', type=str)
+    parser.add_argument("--year", default=2021, type=int)
+    parser.add_argument("--train_month", default=1, type=int)
+    parser.add_argument("--valid_month", default=2, type=int)
+    args = parser.parse_args()
+
+    source_url = 'https://s3.amazonaws.com/nyc-tlc/trip+data'
+    train_path = f'{source_url}/{args.taxi_type}_tripdata_{args.year:04d}-{args.train_month:02d}.parquet'
+    valid_path = f'{source_url}/{args.taxi_type}_tripdata_{args.year:04d}-{args.valid_month:02d}.parquet'
+
+    run_training(train_path, valid_path)
