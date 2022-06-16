e8ae2a0ef3d494bd8b0ededfa56f29d341a565c4
particle <54845098+particle1331@users.noreply.github.com>
2022-06-16 23:38:18 +0800

    Remove other versions.

--- docs/notebooks/mlops/04-deployment/setup.py
+++ docs/notebooks/mlops/04-deployment/setup.py
@@ -45,13 +45,7 @@ setup(
     license="MIT",
     classifiers=[
         # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
