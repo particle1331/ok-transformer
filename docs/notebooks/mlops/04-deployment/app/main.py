5553786f65f4308062264f6004f1804cc5dab214
particle <54845098+particle1331@users.noreply.github.com>
2022-06-18 06:48:32 +0800

    Load model from S3.

--- docs/notebooks/mlops/04-deployment/app/main.py
+++ docs/notebooks/mlops/04-deployment/app/main.py
@@ -1,24 +1,26 @@
 import joblib
 import pandas as pd
+import os
 
-from ride_duration.predict import load_model, make_prediction, prepare_features
+from ride_duration.predict import load_model, make_prediction
 from flask import Flask, request, jsonify
 
 
-model = load_model()
+model, run_id = load_model()
 app = Flask('duration-prediction')
 
 
 @app.route('/predict', methods=['POST'])
 def predict_endpoint():
+    """Predict ride duration using NYCRideDurationModel."""
+    
     ride = request.get_json()
     preds = make_prediction(model, ride)
 
-    result = {
-        'duration': float(preds[0])
-    }
-
-    return jsonify(result)
+    return jsonify({
+        'duration': float(preds[0]),
+        'model_version': run_id,
+    })
 
 
 if __name__ == "__main__":
--- docs/notebooks/mlops/04-deployment/ride_duration/predict.py
+++ docs/notebooks/mlops/04-deployment/ride_duration/predict.py
@@ -1,21 +1,51 @@
 import joblib
 import mlflow
 import pandas as pd
+import os 
+import requests
+
 from ride_duration.utils import package_dir, prepare_features
 from typing import Union
+from mlflow.tracking import MlflowClient
 
 
 def load_model():
-    return joblib.load(package_dir / 'pipeline.pkl')
+    """Get latest production model from tracking server 
+    or specific model from S3 bucket if server is down."""
+
+    try:
+        TRACKING_SERVER_HOST = os.getenv('TRACKING_SERVER_HOST')
+        TRACKING_URI = f"http://{TRACKING_SERVER_HOST}:5000"
+
+        # Check availability of API
+        response = requests.head(TRACKING_URI)
+        if response.status_code != 200:
+            raise Exception(f"Tracking server unavailable: HTTP response code {response.status_code}")
+            
+    except:
+        EXPERIMENT_ID = os.getenv('EXPERIMENT_ID')
+        RUN_ID = os.getenv('MODEL_RUN_ID')
+        
+        print(f"Downloading model {RUN_ID}...")
+        source = f"s3://mlflow-models-ron/{EXPERIMENT_ID}/{RUN_ID}/artifacts/model"
+
+    else:
+        mlflow.set_tracking_uri(TRACKING_URI)
+        client = MlflowClient(tracking_uri=TRACKING_URI)
+        logged_model = client.get_latest_versions(name='NYCRideDurationModel', stages=['Production'])[0]
+        
+        print("Downloading model {RUN_ID} (latest, production)...")
+        RUN_ID = logged_model.run_id
+        source = logged_model.source
+    
+    model = mlflow.pyfunc.load_model(source)
+    return model, RUN_ID
 
 
-def load_mlflow_model(run_id):
-    logged_model = f's3://mlflow-models-ron/1/{run_id}/artifacts/model'
-    model = mlflow.pyfunc.load_model(logged_model)
-    return model
-
-
-def make_prediction(model, input: Union[list[dict], pd.DataFrame]):
-    X = prepare_features(input)
+def make_prediction(model, input_data: Union[list[dict], pd.DataFrame]):
+    """Make prediction from features dict or DataFrame."""
+    
+    X = prepare_features(input_data)
     preds = model.predict(X)
+
     return preds
