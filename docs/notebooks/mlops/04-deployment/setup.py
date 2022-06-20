8edae4a5c88618238550fa319203a7f3f7f690f4
particle <54845098+particle1331@users.noreply.github.com>
2022-06-20 13:31:15 +0800

    Fix comments setup.

--- docs/notebooks/mlops/04-deployment/setup.py
+++ docs/notebooks/mlops/04-deployment/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 
 # Package meta-data.
-NAME = 'ride-duration-prediction'
+NAME = "ride-duration-prediction"
 DESCRIPTION = ""
 URL = ""
 EMAIL = ""
@@ -11,8 +11,8 @@ AUTHOR = ""
 REQUIRES_PYTHON = ">=3.9.0"
 
 
-# The rest you shouldn't have to touch too much. :)
-# Except, perhaps the License and Trove Classifiers!
+# The rest you shouldn"t have to touch too much. Except for install_requires=[]. 
+# Perhaps also the License and Trove Classifiers if publishing to PyPI (public).
 # ------------------------------------------------
 
 long_description = DESCRIPTION
@@ -20,7 +20,7 @@ long_description = DESCRIPTION
 # Load the package's VERSION file as a dictionary.
 about = {}
 ROOT_DIR = Path(__file__).resolve().parent
-PACKAGE_DIR = ROOT_DIR / 'ride_duration'
+PACKAGE_DIR = ROOT_DIR / "ride_duration"
 with open(PACKAGE_DIR / "VERSION") as f:
     _version = f.read().strip()
     about["__version__"] = _version
