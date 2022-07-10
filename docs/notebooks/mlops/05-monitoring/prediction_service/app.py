4d53f50eccb9ea2b398ffc3ca47f27b09a087fbc
Ubuntu <ubuntu@ip-172-31-17-129.ec2.internal>
2022-07-10 07:30:05 +0000

    Uncommenting.

--- docs/notebooks/mlops/05-monitoring/prediction_service/app.py
+++ docs/notebooks/mlops/05-monitoring/prediction_service/app.py
@@ -10,11 +10,11 @@ model = joblib.load('model.bin')
 RUN_ID = 'github/88b935d45baabccde665d3ec0f1d17cfa7e15cab'
 app = Flask('duration-prediction')
 
-# EVIDENTLY_SERVICE_ADDRESS = os.getenv('EVIDENTLY_SERVICE', 'http://127.0.0.1:5000')
-# MONGODB_ADDRESS = os.getenv('MONGODB_ADDRESS', 'mongodb://127.0.0.1:27017')
-# mongo_client = MongoClient(MONGODB_ADDRESS)
-# db = mongo_client.get_database("prediction-service")
-# collection = db.get_collection("data")
+EVIDENTLY_SERVICE_ADDRESS = os.getenv('EVIDENTLY_SERVICE', 'http://127.0.0.1:5000')
+MONGODB_ADDRESS = os.getenv('MONGODB_ADDRESS', 'mongodb://127.0.0.1:27017')
+mongo_client = MongoClient(MONGODB_ADDRESS)
+db = mongo_client.get_database("prediction-service")
+collection = db.get_collection("data")
 
 
 @app.route('/predict', methods=['POST'])
@@ -28,22 +28,22 @@ def predict_endpoint():
         'model_version': RUN_ID,
     }
 
-    # save_to_db(record, pred)
-    # send_to_evidently_service(record, pred)
+    save_to_db(record, pred)
+    send_to_evidently_service(record, pred)
 
     return jsonify(result)
 
 
-# def save_to_db(record, prediction):
-#     rec = record.copy()
-#     rec['prediction'] = prediction
-#     collection.insert_one(rec)
+def save_to_db(record, prediction):
+    rec = record.copy()
+    rec['prediction'] = prediction
+    collection.insert_one(rec)
 
 
-# def send_to_evidently_service(record, prediction):
-#     rec = record.copy()
-#     rec['prediction'] = prediction
-#     requests.post(f"{EVIDENTLY_SERVICE_ADDRESS}/iterate/taxi", json=[rec])
+def send_to_evidently_service(record, prediction):
+    rec = record.copy()
+    rec['prediction'] = prediction
+    requests.post(f"{EVIDENTLY_SERVICE_ADDRESS}/iterate/taxi", json=[rec])
 
 
 if __name__ == "__main__":
