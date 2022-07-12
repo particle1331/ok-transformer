9649c1caf664ab5fc3a17b0665bbe6aab66541ea
particle <54845098+particle1331@users.noreply.github.com>
2022-07-12 15:07:11 +0800

    Refactor directories.

--- docs/notebooks/mlops/05-monitoring/prepare.py
+++ docs/notebooks/mlops/05-monitoring/prepare.py
@@ -1,16 +1,21 @@
 from tqdm import tqdm
 import requests
 
-files = [("green_tripdata_2022-01.parquet", "."), ("green_tripdata_2021-01.parquet", "./evidently_service/datasets")]
+files = [
+    ("fhv_tripdata_2021-01.parquet", "./evidently_service/datasets"),
+    ("fhv_tripdata_2021-12.parquet", ".")
+]
 
 print(f"Download files:")
 for file, path in files:
-    url = f"https://nyc-tlc.s3.amazonaws.com/trip+data/{file}"
+    url = f"https://raw.githubusercontent.com/alexeygrigorev/datasets/master/nyc-tlc/fhv/{file}"
     resp = requests.get(url, stream=True)
     save_path = f"{path}/{file}"
     with open(save_path, "wb") as handle:
-        for data in tqdm(resp.iter_content(),
-                         desc=f"{file}",
-                         postfix=f"save to {save_path}",
-                         total=int(resp.headers["Content-Length"])):
+        for data in tqdm(
+            resp.iter_content(),
+            desc=f"{file}",
+            postfix=f"save to {save_path}",
+            total=int(resp.headers["Content-Length"])
+        ):
             handle.write(data)
