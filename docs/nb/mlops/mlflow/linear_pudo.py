f31f672d3fb0a9752a8f6b25e5f6a822a3988fbb
particle1331 <54845098+particle1331@users.noreply.github.com>
2023-06-04 20:43:10 +0800

    Add mlflow scripts

---
+++ docs/nb/mlops/mlflow/linear_pudo.py
@@ -0,0 +1,67 @@
+import time
+
+import mlflow
+from utils import fixtures, create_pipeline, setup_experiment
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression
+
+from ride_duration.utils import plot_duration_histograms
+from ride_duration.config import config
+from ride_duration.processing import prepare_features
+
+
+def add_endpoints_interaction(df):
+    df["PU_DO"] = df["PULocationID"] + "_" + df["DOLocationID"]
+    return df
+
+
+setup_experiment()
+data = fixtures()
+
+with mlflow.start_run():
+    train_data_path = data["train_data_path"]
+    valid_data_path = data["valid_data_path"]
+    train_data = data["train_data"]
+    valid_data = data["valid_data"]
+
+    # Feature engg + selection
+    transforms = [
+        add_endpoints_interaction,
+        lambda df: df[["PU_DO"] + config.NUM_FEATURES],
+    ]
+
+    X_train, y_train = prepare_features(train_data, transforms, train=True)
+    X_valid, y_valid = prepare_features(valid_data, transforms, train=True)
+
+    # Train model
+    model = LinearRegression()
+    pipe = create_pipeline(model)
+    pipe.fit(X_train, y_train)
+
+    # Computing metrics
+    start_time = time.time()
+    yp_train = pipe.predict(X_train)
+    yp_valid = pipe.predict(X_valid)
+    inference_time = time.time() - start_time
+
+    rmse_train = mean_squared_error(y_train, yp_train, squared=False)
+    rmse_valid = mean_squared_error(y_valid, yp_valid, squared=False)
+
+    fig = plot_duration_histograms(y_train, yp_train, y_valid, yp_valid)
+
+    # Tags
+    mlflow.set_tag("author", "particle")
+    mlflow.set_tag("model", "linear-pudo")
+
+    # MLflow logging
+    mlflow.log_param("train_data_path", train_data_path)
+    mlflow.log_param("valid_data_path", valid_data_path)
+
+    mlflow.log_metric("rmse_train", rmse_train)
+    mlflow.log_metric("rmse_valid", rmse_valid)
+    mlflow.log_metric(
+        "inference_time",
+        inference_time / (len(yp_train) + len(yp_valid)),
+    )
+
+    mlflow.log_figure(fig, "plot.svg")
---
+++ docs/nb/mlops/mlflow/utils.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+
+import mlflow
+import pandas as pd
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.feature_extraction import DictVectorizer
+
+from ride_duration.utils import convert_to_dict
+
+ROOT_DIR = Path(__file__).parents[2]
+DATA_DIR = ROOT_DIR / "data"
+EXPERIMENT_NAME = "nyc-green-taxi"
+EXPERIMENT_DIR = ROOT_DIR / "experiments" / EXPERIMENT_NAME
+TRACKING_URI = f"http://127.0.0.1:5001"
+
+
+def setup_experiment():
+    mlflow.set_tracking_uri(TRACKING_URI)
+    mlflow.set_experiment(EXPERIMENT_NAME)
+
+
+def fixtures():
+    train_data_path = DATA_DIR / "green_tripdata_2021-01.parquet"
+    valid_data_path = DATA_DIR / "green_tripdata_2021-02.parquet"
+    train_data = pd.read_parquet(train_data_path)
+    valid_data = pd.read_parquet(valid_data_path)
+
+    return {
+        "train_data_path": train_data_path,
+        "valid_data_path": valid_data_path,
+        "train_data": train_data,
+        "valid_data": valid_data,
+    }
+
+
+def create_pipeline(model):
+    return make_pipeline(
+        FunctionTransformer(convert_to_dict),
+        DictVectorizer(),
+        model,
+    )
+
+
+def create_feature_pipeline():
+    return make_pipeline(
+        FunctionTransformer(convert_to_dict),
+        DictVectorizer(),
+    )
