680ac732613bf0954e0f055542fbc3fdfdb43220
particle1331 <54845098+particle1331@users.noreply.github.com>
2024-06-19 06:29:57 +0800

    .

--- docs/nb/apis/01/main.py
+++ docs/nb/apis/01/main.py
@@ -6,7 +6,6 @@ app = FastAPI()
 items = [
     {"id": 1, "name": "Item 1", "description": "Description 1", "price": 10.0},
     {"id": 2, "name": "Item 2", "description": "Description 2", "price": 20.0},
-    {"id": 3, "name": "Item 3", "description": "Description 3", "price": 30.0},
 ]
 
 @app.get("/items")
