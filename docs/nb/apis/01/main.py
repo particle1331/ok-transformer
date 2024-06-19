079fa793fd13a11d036f9de0f784abd639b453ed
particle1331 <54845098+particle1331@users.noreply.github.com>
2024-06-20 03:10:48 +0800

    .

--- docs/nb/apis/01/main.py
+++ docs/nb/apis/01/main.py
@@ -1,22 +1,14 @@
-from fastapi import FastAPI, Query
+import numpy as np
+from PIL import Image
+from fastapi import FastAPI, UploadFile
 
 app = FastAPI()
 
-# Sample data
-items = [
-    {"id": 1, "name": "Item 1", "description": "Description 1", "price": 10.0},
-    {"id": 2, "name": "Item 2", "description": "Description 2", "price": 20.0},
-]
-
-
-@app.get("/items")
-def read_items(fields: list[str] = Query(None)):
-    if not fields:
-        return items
-    
-    result = []
-    for item in items:
-        filtered_item = {field: item[field] for field in fields if field in item}
-        result.append(filtered_item)
-    
-    return result
+@app.post("/files")
+async def upload_file(file: UploadFile):
+    img = Image.open(file.file)
+    return {
+        "filename": file.filename,
+        "size": file.size,
+        "dims": np.array(img).shape, 
+    }
